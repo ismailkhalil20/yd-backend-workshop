84eb96351960bde3c1fd34ea1b88aca659df62fe
Martin Najjar <martin_najjar@yahoo.com>
2021-08-05 19:57:15 +0300

    Build the Home page

--- fronted/src/pages/Home.js
+++ fronted/src/pages/Home.js
@@ -1,6 +1,46 @@
-import { Typography } from '@material-ui/core';
+import { Grid, makeStyles, TextField } from '@material-ui/core';
+import homePageImage from '../assets/imgs/university.png';
+
+const useStyles = makeStyles({
+  gridMargin: {
+    marginTop: 15,
+  },
+
+  imageWidth: {
+    width: '100%',
+    maxWidth: 700,
+  },
+
+  inputWidth: {
+    width: '100%',
+    maxWidth: 600,
+    marginTop: 10,
+  },
+});
 
 const Home = () => {
-  return <Typography>Hello World</Typography>;
+  const classes = useStyles();
+
+  return (
+    <Grid
+      className={classes.gridMargin}
+      container
+      justifyContent="center"
+      alignItems="center"
+      direction="column"
+    >
+      <img
+        className={classes.imageWidth}
+        src={homePageImage}
+        alt="University clipart"
+      />
+      <TextField
+        className={classes.inputWidth}
+        type="search"
+        label="Choose a city"
+      />
+    </Grid>
+  );
 };
+
 export default Home;
