1e7c8f33c74a0008f08303c1905e259dba3ddd81
unknown <ismail.th.khalil@gmail.com>
2021-08-06 20:02:38 +0300

    fix errors

--- site/routes/university.js
+++ site/routes/university.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
 const addUniversityControllers = require("../controllers/University");
 
 router.post("/get-university", addUniversityControllers.getUniversities);
-router.post("/add-university", addUniversityControllers.postAddUniversity);
+router.post("/add-university", isAuth, addUniversityControllers.postAddUniversity);
 
 module.exports = router;
