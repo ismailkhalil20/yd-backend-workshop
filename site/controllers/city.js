9ac73568be04cb6f20a286ddc29717ced138a578
fatima katrib <73742127+fatima-katrib@users.noreply.github.com>
2021-08-06 19:44:25 +0300

    Cities controllers/fatima (#10)
    * Update city.js

--- site/controllers/city.js
+++ site/controllers/city.js
@@ -1,13 +1,25 @@
 const City = require("../models/city");
 
-exports.getAddCity = async (req, res, next) => {
+exports.getCity = async (req, res, next) => {
+
   const city = await City.findAll();
   console.log(req.city);
-  res.send(city);
+  res.json(city);
+};
+
+exports.getAddCity = (req, res, next) => {
+  res.render("addCity.jsx");
+  //name of the file may vary//
 };
 
 exports.postAddCity = async (req, res, next) => {
-  const city = await City.findAll();
-  console.log(req.city);
-  res.send(city);
-};
+    console.log(req.body);
+    const cityName = req.body.cityName;
+    const city = City.build({ cityName });
+    try {
+      await city.save();
+      res.json(city);
+    } catch (err) {
+      res.status(400).send(err);
+    }
+  };
