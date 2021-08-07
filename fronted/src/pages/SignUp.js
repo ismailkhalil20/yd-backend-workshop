15448d4be025bf483619cb6c4e8dd30bdbd463eb
fatima katrib <fatimakatrib19@gmail.com>
2021-08-07 11:11:41 +0300

    Change design of sign up page

--- fronted/src/pages/SignUp.js
+++ fronted/src/pages/SignUp.js
@@ -1,7 +1,30 @@
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
+import {Typography, Button, TextField, Container, Grid} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  container: {
+    padding: theme.spacing(3)
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+}));
 
 function SignUn({ handleFetch }) {
+  const classes = useStyles();
   const [user, setUser] = useState({
     email: "",
     password: "",
@@ -37,53 +60,99 @@ function SignUn({ handleFetch }) {
     }
   };
   return (
-    <div>
-      <form onSubmit={handlSingUn}>
-        <div>
-          <label htmlFor="firstName">
-            First Name
-            <input
-              type="text"
+    <Container component="main" className={classes.container} maxWidth="xs">
+    <CssBaseline />
+    <div className={classes.paper}>
+      <Typography variant="h3" gutterBottom>
+        Sign Up
+      </Typography>
+      <form
+        onSubmit={handlSingUn}
+        className={classes.form}
+        noValidate
+        autoComplete="off"
+      >
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              autoComplete="fname"
               name="firstName"
+              variant="outlined"
               required
+              fullWidth
+              id="firstName"
+              color="primary"
+              label="First Name"
+              autoFocus
               value={user.firstName}
               onChange={handlChange}
             />
-          </label>
-          <label htmlFor="firstName">
-            Last Name
-            <input
-              type="text"
-              name="lastName"
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              variant="outlined"
               required
+              fullWidth
+              id="lastName"
+              label="Last Name"
+              color="primary"
+              name="lastName"
+              autoComplete="lname"
               value={user.lastName}
               onChange={handlChange}
             />
-          </label>
-          <label htmlFor="email">
-            Email
-            <input
-              type="email"
-              name="email"
-              required
-              value={user.email}
-              onChange={handlChange}
-            />
-          </label>
-          <label htmlFor="password">
-            Password
-            <input
-              type="password"
-              name="password"
-              required
-              value={user.password}
-              onChange={handlChange}
-            />
-          </label>
-        </div>
-        <button type="submit">Sign Up</button>
+            <Grid item xs={12}>
+              <TextField
+                id="outlined-basic"
+                label="Your Email"
+                variant="outlined"
+                color="primary"
+                fullWidth
+                // ref={register}
+                type="email"
+                name="email"
+                Required
+                value={user.email}
+                onChange={handlChange}
+              ></TextField>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                // ref={register}
+                id="outlined-password-input"
+                label="Password"
+                variant="outlined"
+                color="primary"
+                fullWidth
+                type="password"
+                name="password"
+                Required
+                autoComplete="current-password"
+                value={user.password}
+                onChange={handlChange}
+              ></TextField>
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              className={classes.submit}
+              color="primary"
+              variant="contained"
+              type="submit"
+            >
+              sign in
+            </Button>
+          </Grid>
+        </Grid>
+        <Grid container justifyContent="flex-end">
+          <Grid item>
+            
+            Already have an account? <Link href="/sign-in" variant="body2">Sign in</Link>
+          </Grid>
+        </Grid>
       </form>
     </div>
+  </Container>
   );
 }
 
