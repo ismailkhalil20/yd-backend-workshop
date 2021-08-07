08eea6b2dd7dc7ecc6b06c51a7d31a030671c0ef
Tahany-Alswoidy <tahaniswoidy1232018@gmail.com>
2021-08-07 13:36:43 +0300

    create the favoriteUniversity routes

---
+++ site/routes/favoriteUniversity.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const router = express.Router();
+
+const addFavoriteUniversityControllers = require("../controllers/favoriteUniversity");
+
+router.get("/get-favoriteUniversity", addFavoriteUniversityControllers.getFavoriteUniversity);
+router.post("/add-favoriteUniversity", isAuth, addFavoriteUniversityControllers.postAddFavoriteUniversity);
+router.delete("/delete-favoriteUniversity", isAuth, addFavoriteUniversityControllers.postDeleteFavoriteUniversity);
+
+module.exports = router;
